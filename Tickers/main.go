8dc958af5128d5fde06c8101a30674fe3d41acbc
farid <deltaburs@gmail.com>
2026-05-30 10:27:54 +0330

    Tickers in golang

---
+++ Tickers/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticke := time.NewTicker(500 * time.Millisecond)
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticke.C:
+				fmt.Println("Tick at", t)
+			}
+		}
+	}()
+
+	time.Sleep(2600 * time.Millisecond)
+
+	ticke.Stop()
+	done <- true
+
+	fmt.Println("Ticker stopped")
+}
